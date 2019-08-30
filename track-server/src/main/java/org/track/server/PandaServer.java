cba50a5d7d0b658336b692386a64e3d8d60e9e05
linwbai <linwbai@bailinwei.com>
2019-08-30 23:01:39 +0800

    server

---
+++ track-server/src/main/java/org/track/server/PandaServer.java
@@ -0,0 +1,13 @@
+package org.track.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PandaServer.class, args);
+    }
+
+}
---
+++ track-server/src/main/java/org/track/server/collect/web/HttpCollect.java
@@ -0,0 +1,9 @@
+package org.track.server.collect.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("collect/http")
+public class HttpCollect {
+}
